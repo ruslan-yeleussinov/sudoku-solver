38d5d6b929d8c7bd4a88e3da33de456e85d78362
Ruslan Yeleussinov <ruslan_yeleussinov@mail.ru>
2024-05-14 13:50:56 +0600

    Completed code

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ class Board:
     def __init__(self, board):
         self.board = board
 
+    def __str__(self):
+        board_str = ''
+        for row in self.board:
+            row_str = [str(i) if i else '*' for i in row]
+            board_str += ' '.join(row_str)
+            board_str += '\n'
+        return board_str
+
     def find_empty_cell(self):
         for row, contents in enumerate(self.board):
             try:
@@ -26,6 +34,34 @@ class Board:
                     return False
         return True
 
+    def is_valid(self, empty, num):
+        row, col = empty
+        valid_in_row = self.valid_in_row(row, num)
+        valid_in_col = self.valid_in_col(col, num)
+        valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
+
+    def solver(self):
+        if (next_empty := self.find_empty_cell()) is None:
+            return True
+        for guess in range(1, 10):
+            if self.is_valid(next_empty, guess):
+                row, col = next_empty
+                self.board[row][col] = guess
+                if self.solver():
+                    return True
+                self.board[row][col] = 0
+        return False
+
+def solve_sudoku(board):
+    gameboard = Board(board)
+    print(f'Puzzle to solve:\n{gameboard}')
+    if gameboard.solver():
+        print(f'Solved puzzle:\n{gameboard}')
+    else:
+        print('The provided puzzle is unsolvable.')
+    return gameboard
+
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
   [0, 0, 0, 0, 0, 3, 7, 6, 2],
@@ -37,8 +73,6 @@ puzzle = [
   [0, 0, 0, 5, 1, 9, 0, 0, 8],
   [1, 7, 0, 0, 0, 6, 0, 0, 5]
 ]
+solve_sudoku(puzzle)
 
-gameboard = Board(puzzle)
-print(gameboard.valid_in_square(0, 0, 3))
-
-# At this stage the code checks whether the number is valid in 3x3 square
+# Completed code
